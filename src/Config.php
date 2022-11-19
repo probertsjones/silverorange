931915451d80723029f2005e163ce593b6c164f2
Phil Jones <probertsjones@gmail.com>
2022-11-19 15:40:11 -0800

    Initial commit

---
+++ src/Config.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace silverorange\DevTest;
+
+class Config
+{
+    public string $dsn = 'pgsql:host=10.0.0.171;port=5532;dbname=silverorange;user=silverorange;password=silverorange';
+}
