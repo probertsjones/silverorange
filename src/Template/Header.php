9c5b47c82ed7d3725249a08099311c57cc6e18c8
Phil Jones <probertsjones@gmail.com>
2022-11-19 19:19:29 -0800

    Tidy up post layout and styles

--- src/Template/Header.php
+++ src/Template/Header.php
@@ -22,6 +22,9 @@ class Header implements Template
         <link rel="icon" href="/images/favicon.ico" type="image/x-icon" />
         <link rel="icon" href="/images/icon.svg" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/images/apple-touch-icon.png" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, minimum-scale=1, maximum-scale=1" />
         <meta name="mobile-web-app-capable" content="yes" />
--- src/Template/PostDetails.php
+++ src/Template/PostDetails.php
@@ -10,12 +10,14 @@ class PostDetails extends Layout
     {
         // @codingStandardsIgnoreStart
         return <<<HTML
-        <h1 class="post-title">{$context->getPostTitle()}</h1>
-        <div class="post-content-meta">
-            By: {$context->getPostAuthorName()} @ {$context->getPostCreatedDate()}
+        <div class="post">
+            <h1 class="post-title">{$context->getPostTitle()}</h1>
+            <div class="post-content-meta">
+                By: <span class="post-author"><a href="/author/{$context->getPostAuthorId()}">{$context->getPostAuthorName()}</a></span> @ <span class="date">{$context->getPostCreatedDate()}</span>
+            </div>
+            <div class="post-content">{$context->getPostBodyHTML()}</div>
+            <div class="post-footer">Last edited @ <span class="date">{$context->getPostModifiedDate()}</span></div>
         </div>
-        <div class="post-content">{$context->getPostBodyHTML()}</div>
-
 HTML;
         // @codingStandardsIgnoreEnd
     }
