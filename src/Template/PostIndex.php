37e62685dd7e87376fc62747b7b99638f30c98ad
Phil Jones <probertsjones@gmail.com>
2022-11-19 20:15:37 -0800

    Tidy up post list page

--- src/Template/PostIndex.php
+++ src/Template/PostIndex.php
@@ -9,15 +9,17 @@ class PostIndex extends Layout
     protected function renderPage(Context $context): string
     {
 
-        $listFormat = '<li><a href="/posts/{id}">{title}</a><br>By: {author_full_name} @ {created_at}</li>';
+        $listFormat = '<li class="post-list-meta"><a href="/posts/{id}">{title}</a><br>By: {author_full_name} @ {created_at}</li>';
 
 
         // @codingStandardsIgnoreStart
         return <<<HTML
-        <h1>Latest Posts</h1>
-        <pre>
-            {$context->listPosts($listFormat,$context->getPosts())}
-    </pre>
+        <div class="post">
+            <h1>Latest Posts</h1>
+            <ul class="post-list">
+                {$context->listPosts($listFormat,$context->getPosts())}
+            </ul>
+        </div>
 HTML;
         // @codingStandardsIgnoreEnd
     }
